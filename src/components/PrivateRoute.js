f3fb29561ceb5f93cec770369612724fbb6a0147
Flames-ui <anointedflames27@gmail.com>
2025-05-30 20:54:18 +0100

    PrivateRoute.js

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -2,13 +2,10 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 
-// Protects a route by checking if user is authenticated
-export default function PrivateRoute({ children }) {
+const PrivateRoute = ({ children }) => {
   const { user } = useAuth();
 
-  if (!user) {
-    return <Navigate to="/login" />;
-  }
+  return user ? children : <Navigate to="/login" />;
+};
 
-  return children;
-}
+export default PrivateRoute;
